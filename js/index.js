195f86fc6ddab5b592278dc2594701cda9992194
pedroid <ntuyusheng@gmail.com>
2019-09-29 23:24:24 +0800

    20190929_2

--- js/index.js
+++ js/index.js
@@ -3,6 +3,26 @@ $(document).ready(function() {
   var inst_managerZ = new ManagerZ();  
   var inst_managerUI = new ManagerUI();  
   inst_managerZ.test();
+  turn_off_layout_view();
   
   
 });
+
+function turn_off_layout_view(){
+	for(i=1;i<9;i++){
+		$('#LW_P'+i+'t_Zp'+i+'t2n'+i+'t').css({
+			display: "none"
+		});
+	}
+	
+}
+
+
+function turn_on_layout_view(){
+	for(i=1;i<9;i++){
+		$('#LW_P'+i+'t_Zp'+i+'t2n'+i+'t').css({
+			display: ""
+		});
+	}
+	
+}
